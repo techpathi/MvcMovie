193d777f22c97dd83002e12f1ca37234cedb5841
Venkatapathi Raju Gangiri <venkat4coding@gmail.com>
2019-06-24 09:33:43 +0530

    Add functionality for displaying movies list and add, edit and delete a movie

---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public Movie()
+        {
+
+        }
+    }
+}
